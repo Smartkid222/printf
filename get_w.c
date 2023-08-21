410cdc9c1d1e7de5081464ec9427066cf4c04003
Smartkid222 <emekam848@gmail.com>
2023-08-21 01:39:44 -0700

    get_w.c

---
+++ get_w.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * get_w - Calculates the width for printing
+ * @format: Formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: w.
+ */
+int get_w(const char *format, int *i, va_list list)
+{
+	int current_i;
+	int w = 0;
+
+	for (current_i = *i + 1; format[current_i] != '\0'; current_i++)
+	{
+		if (is_digit(format[current_i]))
+		{
+			w *= 10;
+			w += format[current_i] - '0';
+		}
+		else if (format[current_i] == '*')
+		{
+			current_i++;
+			w = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = current_i - 1;
+
+	return (w);
+}
--- main.h
+++ main.h
@@ -8,5 +8,7 @@ int _printf(const char *format, ...);
 void print_buffer(char buffer[], int *buff_ind);
 int get_f(const char *format, int *i);
 int get_p(const char *format, int *i, va_list list);
+int get_s(const char *format, int *i);
+int get_w(const char *format, int *i, va_list list);
 
 #endif
